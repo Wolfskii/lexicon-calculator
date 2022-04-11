f659306861e64505b2cc492295abd321df43e9ad
Dawid <dawid.kaleta@outlook.com>
2022-04-11 02:02:02 +0200

    Adding tests

--- Calculator.Test/UnitTest1.cs
+++ Calculator.Test/UnitTest1.cs
@@ -7,43 +7,107 @@ namespace Calculator.Tests
     public class CalculatorTests
     {
         [Fact]
-        public void Addition()
+        public void AdditionTest()
         {
             // Arrange
             double a = 24;
             double b = 12;
-            double sum1 = a + b;
-
-            double[] numbers = new double[] { 20, 4, 5, 1 };
-            double sum2 = 30;
+            double sum = a + b;
 
             // Act
-            double addition1 = Calculator.Program.Addition(a, b);
-            double addition2 = Calculator.Program.Addition(numbers);
+            double addition = Calculator.Program.Addition(a, b);
 
             // Assert
-            Assert.Equal(addition1, sum1);
-            Assert.Equal(addition2, sum2);
+            Assert.Equal(addition, sum);
         }
 
         [Fact]
-        public void Subtraction()
+        public void AdditionArrayTest()
+        {
+            // Arrange
+            double[] numbers = new double[] { 20, 4, 5, 1 };
+            double sum = 30;
+
+            // Act
+            double addition = Calculator.Program.Addition(numbers);
+
+            // Assert
+            Assert.Equal(addition, sum);
+        }
+
+        [Fact]
+        public void SubtractionTest()
         {
             // Arrange
             double a = 24;
             double b = 12;
-            double difference1 = a - b;
-
-            double[] numbers = new double[] { 20, 4, 5, 1 };
-            double difference2 = 10;
+            double difference = a - b;
 
             // Act
-            double subtraction1 = Calculator.Program.Subtraction(a, b);
-            double subtraction2 = Calculator.Program.Subtraction(numbers);
+            double subtraction = Calculator.Program.Subtraction(a, b);
 
             // Assert
-            Assert.Equal(subtraction1, difference1);
-            Assert.Equal(subtraction2, difference2);
+            Assert.Equal(subtraction, difference);
+        }
+
+        [Fact]
+        public void SubtractionArrayTest()
+        {
+            // Arrange
+            double[] numbers = new double[] { 20, 4, 5, 1 };
+            double difference = 10;
+
+            // Act
+            double subtraction = Calculator.Program.Subtraction(numbers);
+
+            // Assert
+
+            Assert.Equal(subtraction, difference);
+        }
+
+        [Fact]
+        public void MultiplicationTest()
+        {
+            // Arrange
+            double a = 3;
+            double b = 15;
+            double product = a * b;
+
+            // Act
+            double multiplication = Calculator.Program.Multiplication(a, b);
+
+            // Assert
+            Assert.Equal(multiplication, product);
+        }
+
+        [Fact]
+        public void DivisionTest()
+        {
+            // Arrange
+            double a = 15;
+            double b = 3;
+            double quotient = a / b;
+
+            // Act
+            double division = Calculator.Program.Division(a, b);
+
+            // Assert
+            Assert.Equal(division, quotient);
+        }
+
+        [Fact]
+        public void DivisionZeroTest()
+        {
+            // Arrange
+            double a = 15;
+            double b = 0;
+            double quotient = a / b;
+
+            // Act
+            double division = Calculator.Program.Division(a, b);
+
+            // Assert
+            Assert.Equal(division, quotient);
         }
     }
 }
